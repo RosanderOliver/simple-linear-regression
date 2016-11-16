2c3855f85690a6d4b36a11add6d5a3ce96494b86
olra13 <rosanderoliver@gmail.com>
2016-11-16 15:29:10 +0100

    Fixed reading correct number of datalines

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import sys
 import subprocess
 
 def file_len(fname):
-	p = subprocess.Popen(['wc', '-l', fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	res = p.communicate()
-	if p.returncode != 0:
-		raise IOError(err)
-	res = str(res[0])
-	rows = int(res.split(" ")[0])
-	return rows
+	f = open(fname)
+	rowsInFile = 0
+	for line in f:
+		token = line.split(" ")
+		if token[0] != "@attribute" and token[0] != "@relation" and \
+		token[0].rstrip() != "@data" and token[0] != "\n":
+			rowsInFile = rowsInFile + 1	 
+	return rowsInFile
 
 
 def main(argv):
@@ -35,7 +36,6 @@ def main(argv):
 			data[dataCounter][1] = float(token[1])			
 			dataCounter = dataCounter + 1
 
- 
 	xSum = 0
 	ySum = 0
 	
