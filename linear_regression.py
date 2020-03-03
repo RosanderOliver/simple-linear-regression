ee8fa9a219ce55044208a4c0cba6b3d0950f8d0b
Oliver Rosander <rosanderoliver@gmail.com>
2020-03-03 22:53:43 +0100

    Port to Python3 and clean up code

--- linear_regression.py
+++ linear_regression.py
@@ -1,82 +1,76 @@
 import sys
+import numpy as np
+from pathlib import Path
 
-import subprocess
 
-def file_len(fname):
-	f = open(fname)
-	rowsInFile = 0
-	for line in f:
-		token = line.split(" ")
-		if token[0] != "@attribute" and token[0] != "@relation" and \
-		token[0].rstrip() != "@data" and token[0] != "\n":
-			rowsInFile = rowsInFile + 1	 
-	return rowsInFile
+def file_len(fc):
+	rows_in_file = 0
+	for line in fc:
+		token = line.split(' ')
+		if token[0] != '@attribute' and token[0] != '@relation' and token[0].rstrip() != '@data':
+			rows_in_file = rows_in_file + 1
+	return rows_in_file
 
 
 def main(argv):
-	#Read file and append instances to instance list
-	file = open(argv[0])
-	rowsInFile = file_len(argv[0])
+	# Read file and append instances to instance list
+	fc = Path(argv[0]).read_text()
+	fc = [row for row in ''.join(fc).split('\n') if row != '']
+	rows_in_file = file_len(fc)
 	
-	nrOfAttributes = 0
-	relation = ""
-	dataCounter = 0
+	nr_of_attributes = 0
+	data_counter = 0
+	relation = ''
 
-	for line in file:
-		token = line.split(" ")
+	for line in fc:
+		token = line.split(' ')
 		if token[0] == "@attribute":
-			nrOfAttributes = nrOfAttributes + 1
+			nr_of_attributes = nr_of_attributes + 1
 		elif token[0] == "@relation":
 			relation = token[1]
 		elif token[0].rstrip() == "@data":
-			data = [[0 for x in range(nrOfAttributes)] for y in range (rowsInFile)]
-		elif token[0] != "\n":
+			data = [[0 for x in range(nr_of_attributes)] for y in range(rows_in_file)] # TODO: Change to numpy
+			data = [np.zeros(nr_of_attributes) for _ in range(rows_in_file)] # TODO: Change to numpy
+		else:
 			token = token[0].split(",")
-			data[dataCounter][0] = float(token[0])	
-			data[dataCounter][1] = float(token[1])			
-			dataCounter = dataCounter + 1
+			data[data_counter][0] = float(token[0])
+			data[data_counter][1] = float(token[1])
+			data_counter += 1
 
-	xSum = 0
-	ySum = 0
-	
+	x_sum = y_sum = 0
 	for row in data:
-		xSum += row[0]
-		ySum += row[1]
+		x_sum += row[0]
+		y_sum += row[1]
 
-	print "x sum: " + str(xSum)
-	print "y sum: " + str(ySum)
+	print(f'x sum: {x_sum}')
+	print(f'y sum: {y_sum}')
 
-	xMean = xSum / dataCounter
-	yMean = ySum / dataCounter
+	x_mean = x_sum / data_counter
+	y_mean = y_sum / data_counter
 
-	print "x mean: " + str(xMean)
-	print "y mean: " + str(yMean)
+	print(f'x mean: {x_mean}')
+	print(f'y mean: {y_mean}')
 
-	xxDeviation = 0
-	yyDeviation = 0
-	xyDeviation = 0
+	xx_deviation = yy_deviation = xy_deviation = 0
 	for row in data:
 		if row[0] != 0 and row[1] != 0:
-			tempx = (row[0] - xMean)
-			tempy = (row[1] - yMean)
-			xyDeviation += tempx * tempy
-			xxDeviation += tempx*tempx
-			yyDeviation += tempy*tempy
+			x_deviation = (row[0] - x_mean)
+			y_deviation = (row[1] - y_mean)
+			xy_deviation += x_deviation * y_deviation
+			xx_deviation += x_deviation * x_deviation
+			yy_deviation += y_deviation * y_deviation
 
-	xyDeviation = xyDeviation / (dataCounter - 1)
-	xxDeviation = xxDeviation / (dataCounter - 1)
-	yyDeviation = yyDeviation / (dataCounter -1)
+	xy_deviation = xy_deviation / (data_counter - 1)
+	xx_deviation = xx_deviation / (data_counter - 1)
+	yy_deviation = yy_deviation / (data_counter - 1)
 
+	b = xy_deviation / xx_deviation
+	a = y_mean - b * x_mean
 	
-	b = xyDeviation/xxDeviation
-	a = yMean - b*xMean
-	
-	corr = xyDeviation / ((yyDeviation*xxDeviation) ** 0.5)
+	correlation = xy_deviation / ((yy_deviation * xx_deviation) ** 0.5)
 
-	print "Y = " + str(a) + " + " + str(b) + "x"
-	print "Correlation coefficent: " + str(corr)	
-
-
+	print(f'Y = {str(a)} + {str(b)}x')
+	print(f'Correlation coefficient: {correlation}')
 
 	return 0
 
