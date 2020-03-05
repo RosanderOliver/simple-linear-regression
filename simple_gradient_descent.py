a8368b9a4bf7652e75d845632c5cef2be0301a58
finafiskar <rosanderoliver@gmail.com>
2020-03-06 00:10:58 +0100

    Cleanup code

--- simple_gradient_descent.py
+++ simple_gradient_descent.py
@@ -1,17 +1,20 @@
 import numpy as np
-from typing import Tuple
+from typing import Tuple, Callable, List
 
-def loss_function(dots:Tuple[int, int], weight: np.array):
+
+def loss_function(dots: Tuple[int, int], weight: np.array):
     # Squared error
-    return sum((weight.dot(x) - y) ** 2 for x,y in dots) / len(dots)
+    return sum((weight.dot(x) - y) ** 2 for x, y in dots) / len(dots)
 
-def loss_derivative(dots:Tuple[int, int], weight: np.array):
+
+def loss_derivative(dots: Tuple[int, int], weight: np.array):
     # Derivative in respect to w of squared error
-    return sum((2 * (weight.dot(x) - y) * x) for x,y in dots) / len(dots)
+    return sum((2 * (weight.dot(x) - y) * x) for x, y in dots) / len(dots)
+
 
 lr = 0.01
 
-#Generate data
+# Generate data
 true_weight = np.array([1, 2, 3, 4, 5])
 dim = len(true_weight)
 dots = []
@@ -20,7 +23,9 @@ for i in range(100):
     y = true_weight.dot(x) + np.random.randn()
     dots.append((x, y))
 
-def gradient_descent(loss_function, loss_derivative, dim):
+
+def gradient_descent(loss_function:Callable, loss_derivative:Callable, dots:List[Tuple[np.array,int]]):
+    dim = len(dots[0][0])
     weight = np.zeros(dim)
     for i in range(10):
         loss = loss_function(dots, weight)
@@ -28,5 +33,6 @@ def gradient_descent(loss_function, loss_derivative, dim):
 
         print(f'Loss: {loss}, gradient: {gradient}, weight: {weight}')
         weight = weight - lr * gradient
+
 
 gradient_descent(loss_function, loss_derivative, dim)
