405d7d6c20a2f55723d5e0764d5932c415d35fbe
finafiskar <rosanderoliver@gmail.com>
2020-03-10 21:03:50 +0100

    Add Stochastic Gradient Descent in batches. Add dataclass for storage of data.

--- simple_gradient_descent.py
+++ simple_gradient_descent.py
@@ -1,38 +1,76 @@
 import numpy as np
+import random
+import time
 from typing import Tuple, Callable, List
+from dataclasses import dataclass
+
+random.seed(0)
+
+@dataclass
+class Point:
+    x:np.array
+    y:int
+
+    def __iter__(self):
+        yield self
+
+    def __len__(self):
+        return 1
 
 
-def loss_function(dots: Tuple[int, int], weight: np.array):
+def loss_function(points: Point, weight: np.array):
     # Squared error
-    return sum((weight.dot(x) - y) ** 2 for x, y in dots) / len(dots)
+    return sum((weight.dot(point.x) - point.y) ** 2 for point in points) / len(points)
 
 
-def loss_derivative(dots: Tuple[int, int], weight: np.array):
+def loss_derivative(points: Point, weight: np.array):
     # Derivative in respect to w of squared error
-    return sum((2 * (weight.dot(x) - y) * x) for x, y in dots) / len(dots)
+    return sum((2 * (weight.dot(point.x) - point.y) * point.x) for point in points) / len(points)
 
-
-lr = 0.01
 
 # Generate data
 true_weight = np.array([1, 2, 3, 4, 5])
 dim = len(true_weight)
-dots = []
-for i in range(100):
-    x = np.random.randn(dim).dot(true_weight)
+points = []
+for i in range(10000):
+    x = np.random.randn(dim)
     y = true_weight.dot(x) + np.random.randn()
-    dots.append((x, y))
+    points.append(Point(x=x, y=y))
 
 
-def gradient_descent(loss_function:Callable, loss_derivative:Callable, dots:List[Tuple[np.array,int]]):
-    dim = len(dots[0][0])
+def gradient_descent(loss_function:Callable, loss_derivative:Callable, points:Point, lr=0.01):
+    dim = len(points[0].x)
     weight = np.zeros(dim)
     for i in range(10):
-        loss = loss_function(dots, weight)
-        gradient = loss_derivative(dots, weight)
-
-        print(f'Loss: {loss}, gradient: {gradient}, weight: {weight}')
+        loss = loss_function(points, weight)
+        gradient = loss_derivative(points, weight)
         weight = weight - lr * gradient
 
+        print(f'Iteration: {i}, Loss: {loss}, weight: {weight}')
 
-gradient_descent(loss_function, loss_derivative, dim)
+
+def stochastic_gradient_descent(loss_function:Callable, loss_derivative:Callable, points:Point):
+    dim = len(points[0].x)
+    weight = np.zeros(dim)
+    for i in range(10):
+        # Mini batching with random selection
+        for point in random.choices(points, k=int(len(points)/10)):
+            loss = loss_function(point, weight)
+            gradient = loss_derivative(point, weight)
+            lr = 1 / (len(points)/100)
+            weight = weight - lr * gradient
+
+        print(f'Iteration: {i+1}, Loss: {loss}, weight: {weight}')
+
+
+print('Gradient Descent...')
+start = time.time()
+gradient_descent(loss_function, loss_derivative, points)
+elapsed_time = time.time() - start
+print(f'Elapsed time: {elapsed_time}')
+
+print('\nStochastic Gradient Descent...')
+start = time.time()
+stochastic_gradient_descent(loss_function, loss_derivative, points)
+elapsed_time = time.time() - start
+print(f'Elapsed time: {elapsed_time}')
